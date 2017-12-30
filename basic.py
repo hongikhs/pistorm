a01431169315f82f7ea503c42d73b070646e133a
hongikhs <sw@hongik.hs.kr>
2017-12-30 21:51:00 +0900

    Update basic.py

--- basic.py
+++ basic.py
@@ -2,5 +2,9 @@ from PiStorms import PiStorms
 from time import sleep
 psm = PiStorms()
 def ts():   return psm.BBS1.isTouchedNXT()
+def rs():   return psm.BBS2.lightSensorNXT(reflectiveMode)
+def ls():   return psm.BAS2.lightSensorNXT(reflectiveMode)
+
 while not ts():
-    print('.')
+    print(rs, ls)
+    sleep(0.5)
