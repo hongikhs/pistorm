efa123be620afc42b02bfa2c35565aeb60fbab91
hongikhs <sw@hongik.hs.kr>
2017-12-30 22:08:15 +0900

    Update basic.py

--- basic.py
+++ basic.py
@@ -4,7 +4,13 @@ psm = PiStorms()
 def ts():   return psm.BBS1.isTouchedNXT()
 def rs():   return psm.BBS2.lightSensorNXT(True)
 def ls():   return psm.BAS2.lightSensorNXT(True)
-
+def cm(sp): psm.BBM1.setSpeed(sp)
+def rm(sp): psm.BBM2.setSpeed(sp)
+def lm(sp): psm.BAM2.setSpeed(sp)
+    
 while not ts():
-    print(rs(), ls())
-    sleep(0.5)
+    #print(rs(), ls())
+    rm(30)
+    lm(30)
+rm(0)
+lm(0)
