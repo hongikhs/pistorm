ae57a1167e5738b71768c5ee5a8c0f5899bf0705
hongikhs <sw@hongik.hs.kr>
2018-01-01 02:06:47 +0900

    ok

--- basic.py
+++ basic.py
@@ -1,3 +1,4 @@
+##### setup #####
 from PiStorms import PiStorms
 from time import sleep
 psm = PiStorms()
@@ -6,9 +7,10 @@ def rs():   return psm.BBS2.lightSensorNXT(True)
 def ls():   return psm.BAS2.lightSensorNXT(True)
 cm = psm.BBM1
 rm = psm.BBM2
-lm = psm.BAM2    
-cm.runDegs(-300, 50, True, False)
+lm = psm.BAM2
 
+##### start #####
+cm.runDegs(-300, 50, True, False)
 while not ts():
     rm.setSpeed(30)
     lm.setSpeed(30)
