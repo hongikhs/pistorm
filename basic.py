60365b18cbec315c602dfdfca064fb7adc9c7380
hongikhs <sw@hongik.hs.kr>
2018-01-02 10:38:08 +0900

    Update basic.py

--- basic.py
+++ basic.py
@@ -11,21 +11,24 @@ rm = psm.BBM2
 def touch(s):               return s.isTouchedNXT()
 def light(s, reflected=1):  return s.lightSensorNXT(reflected)
 def motor(m, speed=50):     m.setSpeed(-speed)
-def motorDegree(m, degree, speed=50, brake=True, hold=False):
+def movePos(m, degree, speed=50, brake=True, hold=False):
     m.runDegs(degree, -speed, brake, hold)
     m.waitUntilNotBusy()
-
+def setPos(m, degree):
+    while -5<m.pos()-degree<5:
+        m.setSpeed(degree-m.pos())
+    m.setSpeed(0)
+    
 ##### start #####
-cm.resetPos()
+#cm.resetPos()
 while True:
     if touch(ts):
         motor(lm, 0)
         motor(rm, 0)
-        motorDegree(cm, -300, 100)
+        #motorDegree(cm, -300, 100)
         #motorDegree(cm, 300, 100)
-        while cm.pos()<20: motor(cm, -50)
-        motor(cm, 0)
-        print(cm.pos())
+        setPos(cm, -300)
+        setPos(cm, 0)
     #else:
         #motor(lm)
         #motor(rm)
