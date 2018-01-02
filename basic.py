db5976ec5f77c7ebac9dfad3e7aefc6c0b386a5c
hongikhs <sw@hongik.hs.kr>
2018-01-02 22:43:10 +0900

    Update basic.py

--- basic.py
+++ basic.py
@@ -8,29 +8,25 @@ rs = psm.BBS2
 cm = psm.BAM1
 lm = psm.BAM2
 rm = psm.BBM2
-def touch(s):               return s.isTouchedNXT()
-def light(s, reflected=1):  return s.lightSensorNXT(reflected)
-def motor(m, speed=50):     m.setSpeed(-speed)
-def motorMoveDegs(m, degree, speed=50, brake=True, hold=False):
+def touch(s):                   return s.isTouchedNXT()
+def light(s, reflected=True):   return s.lightSensorNXT(reflected)
+def motor(m, speed=50):         m.setSpeed(-speed)
+def motorRunDegs(m, degree, speed=50, brake=True, hold=True):
     m.runDegs(degree, -speed, brake, hold)
     m.waitUntilNotBusy()
-def motorSetDegs(m, degree, speed=50, brake=True, hold=False):
+def motorSetDegs(m, degree, speed=50, brake=True, hold=True):
+    sleep(0.06)
     sd = m.pos()
-    m.runDegs(degree - m.pos(), speed, brake, hold)
+    m.runDegs(degree - sd, speed, brake, hold)
     m.waitUntilNotBusy()
     
 ##### start #####
-#cm.resetPos()
 while True:
     if touch(ts):
         motor(lm, 0)
         motor(rm, 0)
-        #motorDegree(cm, -300, 100)
-        #motorDegree(cm, 300, 100)
-        setPos(cm, -300)
-        print(cm.pos())
-        setPos(cm, 0)
-        print(cm.pos())
+        motorSetDegs(cm, -300)
+        motorSetDegs(cm, 0)
     #else:
         #motor(lm)
         #motor(rm)
